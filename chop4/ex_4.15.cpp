dfb1442b12d322b687d8e7430f200f0253b32f66
DESKTOP-TE2SDTD\高翔宇 <984931497@qq.com>
2018-10-23 23:14:13 +0800

    第七周 第四章 4.14/4.15 高翔宇

---
+++ chop4/ex_4.15.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int a,b,c,d,e;
+ cout << "Enter account number (or -1 to quit):";
+    cin >> a;
+
+  while ( a £¡= -1 )
+   {
+       cout << "Enter beginning balance:";
+       cin >> b;
+       cout << "Enter total charges:";
+       cin >> c;
+       cout << "Enter total credits:";
+       cin >> d;
+       cout << "Enter credit limit:";
+       cin >> e;
+       b = b + c - d;
+      cout << "New balance is " << b;
+
+      if ( b > e )
+         cout << "Account:      " << a
+              << "Credit limit: " << e
+              << "Balance:      " << b
+              << "Credit Limit Exceeded.";
+
+      cout << "Enter account number (or -1 to quit): ";
+      cin >> a;
+   }
+}
